b34ac2846db7723023a889cdf681b3f29a3e86be
Howard Hu <huhao179790991@gmail.com>
2022-08-13 02:35:19 -0400

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,70 @@
+//TODO: complement 2021 real test questions
+var json = [
+  {
+    "id":2021,
+    "question":"Fluid intelligence is the type of intelligence that has to do with short-term memory and the ability to think quickly, logically, and abstractly in order to solve new problems.It 		1		  in young adulthood, levels out for a period of time, and then 		2		 starts to slowly decline as we age. ",
+    "answer":"1. [A] pauses 	[B] returns	[C] peaks	[D] fades \n 2. [A] alternatively 	[B] formally	[C] accidentally	[D]generally",
+    "explanation":"①句说明流体智力（Fluid intelligence）的含义∶与短期记忆和快速、逻辑、抽象思维能力相关的智力。②句指出，它在青年时期，保持一段时间的平稳，然后开始随年龄增长而逐渐衰退。不难看出②句是在描述流体智力随年龄变化的过程，其中 levels out（升/跌后呈平稳状态）暗示此前存在持续的升/降，starts to slowly decline（开始逐渐衰退）提示此前未曾出现衰退，由此可推知流体智力应该是年少时持续上升，直至青年期增至峰值。此外，由常识也可知，青少年时期人的短期记忆力最强，思维敏捷度最佳。综上，C正确。\n 由上题分析可知，②句是在说明流体智力随年龄变化的规律，具有普遍性、一般性。目由③句核心信息（但）科学家发现大脑某些变化可能并非不可避免也可反推流体智力随变老而衰退只是一般情形。【D】 generally 常用于引出在大多数情况下成立的现象、观点等，符合文意。"
+  },
+  {
+    "id":2021,
+    "question":"But 		3		 aging is inevitable, scientists are finding out that certain changes in brain function may not be. ",
+    "answer":"3. [A] while 	[B] since 	[C]once 	[D]until ",
+    "explanation":"空格句指出，衰老无可避免，科学家们发现大脑功能的某些变化或许并不是这样（并非不可避免）。利用句内情形对比衰老无可避免 vs 大脑功能的某些变化可以避免可推知句子前后为让步一转折逻辑，【A】 while 可用于从句之首，引出与主句内容相对比的信息，符合文意。"
+  },
+  {
+    "id":2021,
+    "question":"One study found that muscle loss and the 		4			of body fat around the abdomen are associated with a decline in fluid intelligence. ",
+    "answer":"3. [A] while 	[B] since 	[C]once 	[D]until",
+    "explanation":" 空格句指出，研究发现肌肉流失及腹部体脂___与流体智力的衰退相关。由句中并列关系可知肌肉流失与腹部体脂__同向，而肌肉（muscle）和脂肪（fat）对立，所以空格词应与loss（失去、流失）反向，表示增加/累积。另外，由首段已知，流体智力随年龄的增长而衰退，而由常识可知随年龄增长，往往会肌肉流失，腹部脂肪堆积。此外，下文多处信息均表明过多的腹部脂肪对流体智力不利（如 higher...abdominal fat...worse...fluid intelligence）。综上三条线索可确定 B正确"
+  },
+  {
+    "id":2021,
+    "question":"This suggests the 		5		 that lifestyle factors might help prevent or 		6		 this type of decline.",
+    "answer":"5. [A] possibility	[B]  decision	[C] goal  	[D] requirement\n6. [A] delay 	[B] ensure	[C] seek 	[D]utilize",
+    "explanation":"空格句指出，这（肌肉流失及腹部体脂堆积与流体智力的衰退有关）表明了一种____生活方式因素可能有助于防止或……这种流体智力的衰退。可见空格名词应聚焦研究意义（Thissuggests the____），体现 that 从句的性质。而从句的整体语义和其中用词 might help，以及主句的suggests均指向一种可能情形。另外，本段两句间实为通过逻辑推理得出可能情形∶由研究发现肌肉流失及腹部体脂堆积会导致流体智力衰退可推知正确的生活方式可以减少肌肉流失和腹部脂肪堆积，从而防止流体智力衰退。综合两方面推理可知【A】 possibility 为正确项。"
+  },   
+  {
+    "id":2021,
+    "question":"The researchers looked at data that 		7		 measurements of lean muscle and abdominal fat from more than 4,000 middle-to-older-aged men and women and		8			 that data to reported changes in fluid intelligence over a six-year period.",
+    "answer":"7. [A] modified 	[B]  supported	[C] included 	[D] predicted \n	8. [A] devoted 	[B]  compared	[C] converted 	[D] applied",
+    "explanation":"空格所在定语从句说明研究者所分析的数据（data）的特征∶（这些数据）____4000 多名中老年男性和女性的精瘦肌肉及腹部脂肪测量值（measurements）。数据（that/data）与测量值（measurements）为同类事物，故空格词只能表示包含关系。另外联系上文可知，本句与上段为说明研究发现—介绍研究数据的对应关系，数据包含研究对象的精瘦肌肉及腹部脂肪测量值恰能对应上文提及的影响流体智力的两个具体因素，确认【C】 included 正确。\n各项均可构成～sth to sth搭配，故需从上下文语义角度辨析。空格所在分句指出，研究人员将这些数据（受试者精瘦肌肉及腹部脂肪测量值）___受试者六年来的流体智力变化。由上段已知研究发现为肌肉流失及腹部体脂堆积与流体智力的衰退相关，而要测定这种相关性必然需要比较/比对相应的数据，由此可知【B】 compared（to）正确。"
+  },
+  {
+    "id":2021,
+    "question":"They found that middle-aged people 						9								higher measures of abdominal fat						1	0									worse on measures of fluid intelligence as the years 							11						.",
+    "answer":"8. [A] devoted 	[B]  compared	[C] converted 	[D] applied\n 9. [A] with 	[B] above	[C]by 	[D] against\n10. [A] lived 	[B]  managed 	[C] scored	[D] played ",
+    "explanation":"空格句以They found 引出研究发现;更高腹部脂肪测量值的中年人流体智力测量值……。结合上句提到的研究方式将 4000 多名中老年人的腹部脂肪测量值和其流体智力变化数据相比对以及上段说明的研究发现流体智力下降与人们的体脂增加相关，可知空格词应表示具有显示出等，【A】 with 可表示拥有或携带某物/具有某种特征，符合文意。\n结合上文可知，空格动词应体现腹部脂肪越多，流体智力越易衰退这一关系以及将腹部脂肪测量值和受试者流体智力变化值进行比较的研究方法，即腹部脂肪测量值越高，流体智力测量得分越低，score＋具体数值或表示好/差的副词及其比较级＋on十某一方面表示在某项指标或测试中的具体得分情况，例∶scored 98/highly/higher on TOEFL（托福测试中得 98 分/得分高/较高），C符合文意。\n空格句大意为;随着岁月，腹部脂肪越多的中年人流体智力越低。结合首段所述流体智力的变化规律随年龄增长而衰退，以及腹部脂肪的通常变化规律随年龄增长而增加，可知空格词应表示（岁月）流逝/（时间）推移，as the years go by 为固定表达，意为随着时光流逝，D 正确。"
+  },
+  {
+    "id":2021,
+    "question":"For women, the association may be 		12			 to changes in immunity that resulted from excess abdominal fat; in men, the immune system did not appear to be 		13		. ",
+    "answer":"12. [A] superior 	[B] attributable 	[C] parallel 	[D] resistant\n 13. [A] restored 	[B] isolated 	[C]involved 	[D] controlled",
+    "explanation":"空格所在分句指出，这种联系（腹部体脂堆积与流体智力衰退相关）可能____腹部脂肪过多导致的免疫力变化。resulted from 提示免疫力变化为腹部体脂堆积→流体智力衰退的中间环节，因此空格词应引出原因，搭建根本原因（腹部脂肪增加）→直接原因（免疫力变化）→最终结果（流体智力衰退）的完整因果链条，【B】 ttributable（to）符合文意。\n句中 For women...attributable to changes in immunity..;in men...the immune system did not appear to...形成平行对仗结构，分别说明女性/男性的免疫力对这种联系（腹部体脂堆积与流体智力衰退相关）的影响，空格词应与 attributable 语义指向一致，表示有因果关联/牵涉等，【C】 involved可表示某物是某种情况/事件/活动的重要参与者或影响因素，符合文意。"
+  },
+  {
+    "id":2021,
+    "question":"It is hoped that future studies could 			14			these differences and perhaps lead to different 		15		 for men and women.",
+    "answer":"14. [A] alter	[B] spread 	[C] remove 	[D] explain\n15. [A] compensations 	[B]  symptoms 	[C] demands 	[D] treatments",
+    "explanation":"空格句指出，希望未来的研究可以 这些差异。这些差异指前一句所述女性身上的这种联系或许（may be）与免疫力变化相关，男性身上的这种联系似乎（appear to）与免疫系统无关。其中 may be、appear to 均带有明显的推测语气，针对男性的内容仅否定了无关因素，并未指明相关因素，暗示还需进一步研究确认。另外，空格句中 It is hoped that future studies could 针对后续的研究方向传达出强烈的建议/期盼。综合两方面线索可知，空格词应表示研究/分析/解释等，以实现当前研究局限—后续研究方向的呼应，【D】 explain 符合文意。\nand perhaps lead to...引出对未来研究的另一项期许∶带来针对男性和女性的不同。结合本句 could...and perhaps lead to 带有的递进语义，上句腹部脂肪与流体智力相关的原因存在性别差异以及而第二段②句研究的根本目的是通过改变生活方式延缓流体智力衰退，可知lead to differentfor men and women 应体现对症下药，即针对男性和女性研制出不同的疗法药物来延缓流体智力衰退，【D】 treatments 符合文意。"
+  },
+  {
+    "id":2021,
+    "question":" 		16			 there are steps you can 		17		 to help reduce abdominal fat and maintain lean muscle mass as you age in order to protect both your physical and mental			18 			",
+    "answer":"16. [A] Likewise 	[B]  Meanwhile 	[C] Therefore 	[D] Instead\n17. [A] change 	[B]  watch 	[C] count 	[D] take\n18. [A] well-being 	[B] process 	[C] formation	[D]coordination",
+    "explanation":"上段末句憧憬未来研究成果（It is hoped that future studies could...），空格句转而向读者提出建议（there are steps you can...），可见空格词应体现平行并列关系，指向两个方面的建议。【B】Meanwhile 意为与此同时，用于引出某种情况/事物的不同方面，符合文意。\n空格词位于嵌套了定语从句的 there be 句型中，从句可改写为 you can _ stepsto...。to后的内容指向目的（减少腹部脂肪，保持肌肉量），可见空格词应表示采取/使用（方法）等，take steps to 为固定搭配，表示采取措施以做到某事，【D】 take 正确。\n空格句中以 to...in order to...构建因果目的链∶采取措施→减少腹部脂肪，保持肌肉量→维护自身的身心。联系贯穿全文的主旨减少腹部脂肪、增加肌肉量可延缓流体智力衰退，可知空格词意在说明采取措施的根本目的（维护身心健康∶减少脂肪，保持肌肉，延缓流体智力衰退）。physical and mental health/well-being 为固定搭配，表示身心健康，【A】well-being 正确。"
+  },
+  {
+    "id":2021,
+    "question":"The two highly recommended lifestyle approaches are maintaining or increasing your				19			 of aerobic exercise and following Mediterranean-style 20 that is high in fiber and eliminates highly processed foods.",
+    "answer":"19. [A] level 	[B] love	[C] knowledge 	[D] space\n20. [A] design	[B] routine 	[C] diet	[D] prescription",
+    "explanation":"空格句指出两种强烈推荐的生活方式;保持或提升你的有氧运动以及……。根据上一句提出的建议可知，空格句说明减少腹部脂肪，保持肌肉量的具体做法。结合常识可知，运动 有助于减脂增肌，故空格词应表示强度/频率/水平等，【A】level 正确。\n本题的解题思路有二∶1.句首两种强烈推荐的生活方式（lifestyle approaches）已为Mediterranean-style____限定方向，选项中只有【B】 routine、【C】 diet 能体现生活方式。2.定语从句that is high in...foods富含纤维，去除精加工食品表明 Mediterranean-style是一种饮食。综上，C为正确项。"
+  },
+]
+
+function getquestion(){
+  return [json[Math.floor(Math.random() * json.length)]]
+}
+module.exports = {
+  testJson: getquestion()
+}
